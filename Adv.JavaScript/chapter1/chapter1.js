911723e4bdb3b510a55a8e13e36b44a0034b65ee
Kundani Nemugumoni <64401025+kundani98@users.noreply.github.com>
2023-04-27 07:50:08 +0200

    Add files via upload

---
+++ Adv.JavaScript/chapter1/chapter1.js
@@ -0,0 +1,7 @@
+//Practice exercise 1.1 :Using Browser console
+//Practice exercise 1.2 :Creating Html
+//Practice exercise 1.3:javascript file 
+//Practice exercise 1.4:Comments 
+//Chapter Project:Creating an HTML file and a linked JavaScript file 
+
+alert("Hello")
