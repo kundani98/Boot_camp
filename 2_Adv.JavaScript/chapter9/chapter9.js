23dae824c6f6627d13c3fe079b95fc40e51e0991
Kundani Nemugumoni <64401025+kundani98@users.noreply.github.com>
2023-04-28 07:46:37 +0200

    Add files via upload

---
+++ 2_Adv.JavaScript/chapter9/chapter9.js
@@ -0,0 +1,26 @@
+//The BOM
+
+
+
+//Practice exercise 9.1 
+
+//Window history object 
+
+//Window navigator object 
+
+//Window location object 
+
+//Practice exercise 9.2 
+
+//The DOM 
+
+//Selecting page elements
+const ele1 = document.querySelector("h1"); 
+console.dir(ele1);
+
+//Practice exercise 9.3 
+
+
+
+
+
